bf36b628719055b603ccd3d7784d6bb8819e456e
ilinoa <ilinoa90@gmail.com>
2019-11-14 01:25:01 +0300

    change appjs for example

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header>
-        <p>
           <div>
             <p>How create file gitignore: .gitignore,  *.txt</p>
             <p>How see log: git log  </p>
-
             <p>    Cancel operations: </p>
             <p> git log --oneline (can you see id everyone) </p>
             <p> git checkout 764570d (heir you may see what files was change [id]) after you input </p>
-          
           </div>
-        </p>
       </header>
     </div>
   );

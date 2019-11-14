d6353d1c1af5df4957ce955deaea41a53812c735
ilinoa <ilinoa90@gmail.com>
2019-11-14 13:58:05 +0300

    hw

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-          <div>
-            <p>How create file gitignore: .gitignore,  *.txt</p>
-            <p>How see log: git log  </p>
-            <p>    Cancel operations: </p>
-            <p> git log --oneline (can you see id everyone) </p>
-            <p> git checkout 764570d (heir you may see what files was change [id]) after you input </p>
-          </div>
-      </header>
-    </div>
-  );
-}
+
+ 
+class App extends Component {
+  render() {
+        return (
+          <h1>Hello World!</h1>
+        );
+      }
+    }
+ 
 
 export default App;

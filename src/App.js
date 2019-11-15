5a99c248346dc4f0124914929a9790a0badf1434
ilinoa <ilinoa90@gmail.com>
2019-11-15 14:32:45 +0300

    test deploy heroku

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ class Apps extends Component {
   render() {
     return(
           <div className="box">  
-            <div className="text">name game: {this.props.game} status: {this.props.status}%</div>
-            <button onClick={this.test} className="btn light">добавить</button>
+            <div className="text">name GAME: {this.props.game} status: {this.props.status}%</div>
+            <button onClick={this.test} className="btn light">добавить</button> 
             <button onClick={this.test} className="btn red">Удалить</button>
           </div>
         );        
